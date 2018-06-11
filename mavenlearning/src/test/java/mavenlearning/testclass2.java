bba55c17743f37ac7104087acc35f27b6ebe3d50
Raj Kamal <rkamal@fb.com>
2018-06-11 17:08:35 +0800

    committ 2

--- mavenlearning/src/test/java/mavenlearning/testclass2.java
+++ mavenlearning/src/test/java/mavenlearning/testclass2.java
@@ -2,18 +2,35 @@ package mavenlearning;
 
 import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class testclass2 {
 
 	@Test
-	public void sum()
+	public void googletest() throws InterruptedException
 	{
-	int a = 10;
-	int b = 20;
-	System.out.println("Sum");
-	AssertJUnit.assertEquals(30, a+b);
+	//System.setProperty("webdriver.chrome.driver","/Users/rkamal/git/testautomationpoc/mavenlearning/chromedriver");
+	
+		 WebDriver driver = new ChromeDriver();
+		  driver.get("http://www.google.com/xhtml");
+		  Thread.sleep(5000);  // Let the user actually see something!
+		  WebElement searchBox = driver.findElement(By.name("q"));
+		  searchBox.sendKeys("ChromeDriver");
+		  searchBox.submit();
+		  Thread.sleep(5000);  // Let the user actually see something!
+		  driver.quit();
+	
 	}
 	
 	@Test
