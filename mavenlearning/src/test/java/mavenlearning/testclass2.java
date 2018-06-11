3f3a99c7c81348ceb549cd0415ca8e25b68affaf
Raj Kamal <rkamal@fb.com>
2018-06-11 15:53:34 +0800

    first commit

---
+++ mavenlearning/src/test/java/mavenlearning/testclass2.java
@@ -0,0 +1,28 @@
+package mavenlearning;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class testclass2 {
+
+	@Test
+	public void sum()
+	{
+	int a = 10;
+	int b = 20;
+	System.out.println("Sum");
+	AssertJUnit.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void sub()
+	{
+	int a = 10;
+	int b = 20;
+	System.out.println("Sub");
+	AssertJUnit.assertEquals(10,b-a);
+	}
+	
+}
